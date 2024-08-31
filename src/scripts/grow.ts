f2902c8a40f38ea4f0476297edc4f4f3ec923580
Stefan Benz <benz.st@gmail.com>
2024-08-31 10:11:52 +0200

    add apramter to run grow on different server

--- src/scripts/grow.ts
+++ src/scripts/grow.ts
@@ -10,28 +10,41 @@ export async function main(ns: NS) {
     hackTarget = ns.args[0];
   }
 
-  if (gainRoot(ns, hackTarget).length) {
-    let now = Date.now();
-    const t = ns.getGrowTime(hackTarget);
-    let end = now + t;
-    let n = Math.floor(ns.growthAnalyze(hackTarget, ns.getServerMaxMoney(hackTarget) / Math.max(1, ns.getServerMoneyAvailable(hackTarget))));
-    let p = ns.run("scripts/do-grow.js", n, hackTarget);
-    while (p == 0 && n >= 2) {
-      n = Math.floor(n / 2);
-      p = ns.run("scripts/do-grow.js", n, hackTarget);
-    }
-    if (p == 0) {
-      ns.tprintf("failed to start grow script");
-      return;
-    }
-    await ns.sleep(100);
-    while (ns.isRunning(p)) {
-      now = Date.now();
-      ns.print("estimated time left: " + ns.tFormat(end - now));
-      await ns.sleep(1000);
-    }
-    ns.print("grow finished");
-  } else {
-    ns.tprint("can't gain root on " + hackTarget);
+  let runTarget = "home";
+  if (ns.args.length > 1 && typeof ns.args[1] === "string") {
+    runTarget = ns.args[1];
   }
+
+  if (!gainRoot(ns, hackTarget).length) {
+    ns.tprint("can't gain root on " + hackTarget);
+    return;
+  }
+  
+  if (runTarget != "home" && !gainRoot(ns, runTarget).length) {
+    ns.tprint("can't gain root on " + runTarget);
+    return;
+  }
+  
+  let now = Date.now();
+  const t = ns.getGrowTime(hackTarget);
+  let end = now + t;
+  let n = Math.ceil(ns.growthAnalyze(hackTarget, ns.getServerMaxMoney(hackTarget) / Math.max(1, ns.getServerMoneyAvailable(hackTarget))));
+  if (!ns.fileExists("scripts/do-grow.js", runTarget)) {
+    ns.scp("scripts/do-grow.js", runTarget);
+  }
+  let p = ns.exec("scripts/do-grow.js", runTarget, n, hackTarget);
+  while (p == 0 && --n >= 1) {
+    p = ns.exec("scripts/do-grow.js", runTarget, n, hackTarget);
+  }
+  if (p == 0) {
+    ns.tprintf("failed to start grow script");
+    return;
+  }
+  await ns.sleep(100);
+  while (ns.isRunning(p)) {
+    now = Date.now();
+    ns.print("estimated time left: " + ns.tFormat(end - now));
+    await ns.sleep(1000);
+  }
+  ns.print("grow finished");
 }
